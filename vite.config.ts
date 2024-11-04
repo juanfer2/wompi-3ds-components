ef8ba6843148b7d41168e086bfec33e3de30ce3e
Juan Fernando Villadiego Madera <64051193+juanfer2@users.noreply.github.com>
2024-11-04 13:24:01 -0500

    update setting vite config

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   plugins: [react(), libInjectCss(), dts({ include: ["lib"] })],
   resolve: {
     alias: {
-      "@": "/src",
+      "@": "./",
     },
   },
   build: {
